9579a50b9647f3189f5c62e41011248d9c47628f
LeDinhPhuc <Phuc092001>
2023-03-04 18:31:33 +0700

    Django RestfullAPI

---
+++ courseapp/courses/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Category, Course, Lesson, Tag
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField(source='image')
+
+    def get_image(self, course):
+        if course.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri('/static/%s' % course.image.name) if request else ''
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'description', 'image', 'created_date', 'category_id']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id', 'subject', 'created_date', 'updated_date', 'image']
+
+
+class LessonDetailsSerializer(LessonSerializer):
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = LessonSerializer.Meta.model
+        fields = LessonSerializer.Meta.fields + ['content', 'tags']
